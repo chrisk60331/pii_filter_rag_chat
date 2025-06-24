ed4a940560d20f91286f01249ec96b376adcfa16
chris <cking@newmathdata.com>
2025-06-24 11:42:15 -0700

    add models

--- aws_pdf_rag_quickstart/src/aws_rag_quickstart/constants.py
+++ aws_pdf_rag_quickstart/src/aws_rag_quickstart/constants.py
@@ -15,7 +15,24 @@ BEDROCK_MODELS = [
     "us.anthropic.claude-3-5-sonnet-20241022-v2:0",
     "us.anthropic.claude-3-7-sonnet-20250219-v1:0",
     "us.anthropic.claude-opus-4-20250514-v1:0",
-    "us.anthropic.claude-sonnet-4-20250514-v1:0"
+    "us.anthropic.claude-sonnet-4-20250514-v1:0",
+    "us.deepseek.r1-v1:0",
+    "us.meta.llama4-maverick-17b-instruct-v1:0",
+    "us.meta.llama4-scout-17b-instruct-v1:0",
+    "us.meta.llama3-1-70b-instruct-v1:0",
+    "us.meta.llama3-1-8b-instruct-v1:0",
+    "us.meta.llama3-2-11b-instruct-v1:0",
+    "us.meta.llama3-2-1b-instruct-v1:0",
+    "us.meta.llama3-2-3b-instruct-v1:0",
+    "us.meta.llama3-2-90b-instruct-v1:0",
+    "us.meta.llama3-3-70b-instruct-v1:0",
+    "us.mistral.pixtral-large-2502-v1:0",
+    "us.amazon.nova-lite-v1:0",
+    "us.amazon.nova-micro-v1:0",
+    "us.amazon.nova-premier-v1:0",
+    "us.amazon.nova-pro-v1:0",
+    "us.writer.palmyra-x4-v1:0",
+    "us.writer.palmyra-x5-v1:0",
 ]
 
 # OpenAI models
@@ -25,8 +42,15 @@ OPENAI_MODELS = [
     "gpt-4",
     "gpt-4-turbo-preview",
     "gpt-4o",
+    "chatgpt-4o",
     "gpt-4o-mini",
     "gpt-4-turbo",
+    "gpt-4.1",
+    "gpt-o3",
+    "gpt-o3-pro",
+    "gpt-o3-mini",
+    "gpt-o1-mini"
+    "gpt-o1-pro"
 ]
 
 # Combined list of all available models
